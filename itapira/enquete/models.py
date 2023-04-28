398548ee11f9273f95ba84c4de4c2bab3e9a6079
Gustavo Brandao Barros <126878240+poderprincipe@users.noreply.github.com>
2023-04-28 10:45:29 -0300

    Atualização
    Atualização da aula do dia 28/04/23,
    projeto Django adicionado perguntas e respostas no site "ENQUETE" , adicionando as migrações para adicionar um Banco de Dados ao projeto.
    
    ADICIONAR o arquivo ".DOCX" para salvar senha e usuário e criar um superUsuario

--- itapira/enquete/models.py
+++ itapira/enquete/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Questao(models.Model):
+    pergunta = models.CharField(max_length=200)
+    data= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.pergunta} - {self.data}" 
+
+
+class Resposta(models.Model):
+    questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
+    resposta = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.resposta
